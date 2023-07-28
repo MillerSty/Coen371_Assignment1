580380c07a979c1ee963fa6f4bce01797ead772f
BlackSound1 <matthewr@videotron.ca>
2023-07-28 15:07:14 -0400

    Initial work

---
+++ src/matt-models/MattArm.cpp
@@ -0,0 +1,75 @@
+#include "MattArm.h"
+
+MattArm::MattArm()
+{
+	groupMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(-20.0f, 1.0f, 20.0f));
+	worldMatrix = glm::mat4(1.0f);
+	initialScale = glm::vec3(1.0f);
+}
+
+void MattArm::setArmColor(glm::vec3 color) { armColor = color; }
+
+glm::vec3 MattArm::getArmColor() { return armColor; }
+
+void MattArm::setGroupMatrix(glm::mat4 groupMat) { groupMatrix = groupMat; }
+
+glm::mat4 MattArm::getGroupMatrix() { return groupMatrix; }
+
+void MattArm::setWorldMatrix(glm::mat4 worldMat) { worldMatrix = worldMat; }
+
+glm::mat4 MattArm::getWorldMatrix() { return worldMatrix; }
+
+void MattArm::setShaderProgram(GLuint shaderProg) { shaderProgram = shaderProg; }
+
+void MattArm::setInitialPosition(glm::vec3 postion) { initialPosition = postion; }
+
+glm::vec3 MattArm::getInitialPosition() { return initialPosition; }
+
+void MattArm::setInitialScale(glm::vec3 scale) { initialScale = scale; }
+
+glm::vec3 MattArm::getInitialScale() { return initialScale; }
+
+GLuint MattArm::getShaderProgram() { return shaderProgram; }
+
+void MattArm::setVAO(int vao) { unitCubeVAO = vao; }
+
+int MattArm::getVAO() { return unitCubeVAO; }
+
+MattArm::drawArm()
+{
+	drawUpperArm();
+	drawLowerArm();
+}
+
+void MattArm::drawUpperArm()
+{
+	glm::mat4 modelMat = worldMatrix * groupMatrix;
+
+	modelMat = glm::scale(modelMat, initialScale);
+	modelMat = glm::translate(modelMat, glm:vec3(2.0f, 3.0f, 0.0f));
+	modelMat = glm::scale(modelMat, glm::vec3(1.0f, 4.0f, 1.0f));
+
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "worldMatrix"), 1, GL_FALSE, &modelMat[0][0]);
+
+	glBindVertexArray(unitCubeVAO);
+
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+void MattArm::drawLowerArm()
+{
+	glm::mat4 modelMat = worldMatrix * groupMatrix;
+
+	modelMat = glm::scale(modelMat, initialScale);
+	modelMat = glm::rotate(modelMat, glm::radians(60.0f), glm::vec3(0.0f, 0.0f, -1.0f));
+	modelMat = glm::scale(modelMat, glm::vec3(1.0f, 4.0f, 1.0f));
+
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "worldMatrix"), 1, GL_FALSE, &modelMat[0][0]);
+
+	glBindVertexArray(unitCubeVAO);
+	
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+}
+
+
+
---
+++ src/matt-models/MattArm.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "GL/glew.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+class MattArm
+{
+private:
+	glm::vec3 armColor;
+	glm::mat4 groupMatrix;
+	glm::mat4 worldMatrix;
+	glm::vec3 initialPosition;
+	glm::vec3 initialScale;
+	GLuint shaderProgram;
+	int unitCubeVAO;
+
+	void drawUpperArm();
+	void drawLowerArm();
+
+public:
+
+	MattArm();
+	//~MattArm();
+
+	void setArmColor(glm::vec3 color);
+	glm::vec3 getArmColor();
+	void setGroupMatrix(glm::mat4 groupMat);
+	glm::mat4 getGroupMatrix();
+	void setWorldMatrix(glm::mat4 worldMat);
+	glm::mat4 getWorldMatrix();
+	void setShaderProgram(GLuint shaderProg);
+	void setInitialPosition(glm::vec3 postion);
+	glm::vec3 getInitialPosition();
+	void setInitialScale(glm::vec3 scale);
+	glm::vec3 getInitialScale();
+	GLuint getShaderProgram();
+	void setVAO(int vao);
+	int getVAO();
+
+	void drawArm();
+};
